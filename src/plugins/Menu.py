0c5c31b60237803292e15ac40961ddec71bc3a27
Harmless-Turtle <a1097740481@163.com>
2025-07-07 00:37:12 +0800

    1

--- src/plugins/Menu.py
+++ src/plugins/Menu.py
@@ -1,4 +1,4 @@
-from nonebot.plugin import on_command  # 导入事件响应器
+from nonebot.plugin import on_command,on_message  # 导入事件响应器
 from nonebot.adapters import Message  # 导入抽象基类Message以允许Bot回复str
 from nonebot.adapters.onebot.v11 import Message, MessageSegment, MessageEvent, GroupMessageEvent  # 导入事件响应器以进行操作
 from nonebot import require
@@ -15,6 +15,7 @@ Main_Menu = on_command("菜单01",aliases={"基本菜单"},priority=99,block=Tru
 Furry_Menu = on_command("菜单02",aliases={"Furry菜单","furry菜单"}, priority=99, block=True)
 Marry_Menu = on_command("菜单03",aliases={"结婚菜单"},priority=99,block=True)
 Service_Menu = on_command("服务条款",aliases={"用户协议"},block=True)
+atmenu = on_message(rule=lambda event: event.is_tome() and "凌辉" in str(event.get_message()), priority=59, block=True)
 
 opendata = Path.cwd()
 All_Menu_Markdown = opendata / 'data/Menu/All_Menu.md'
@@ -32,6 +33,15 @@ async def Menu_Function(event:MessageEvent,args:Message = CommandArg()):
     a.save("md2pic.png", format="PNG")
 
     await Menu.finish(MessageSegment.reply(event.message_id)+MessageSegment.image(pic))
+
+@atmenu.handle()
+async def atmenu_Function(event:MessageEvent,args:Message = CommandArg()):
+    if args.extract_plain_text():
+        await atmenu.finish()    # 若消息后面存在文本则不响应
+    pic = await md_to_pic(md_path=All_Menu_Markdown,width=900)
+    a = Image.open(io.BytesIO(pic))
+    a.save("md2pic.png", format="PNG")
+    await atmenu.finish(MessageSegment.reply(event.message_id)+MessageSegment.image(pic))
 
 @Main_Menu.handle()
 async def Main_Menu_Function(event:MessageEvent,args:Message = CommandArg()):
