1aca4abe6c29ef9fcffdf28af2a7fca2d3992d8a
票票 <634823896@qq.com>
2025-09-27 00:11:46 +0800

    优化菜单命令路径定义，优化代码结构
    Signed-off-by: 票票 <634823896@qq.com>

--- src/plugins/menu.py
+++ src/plugins/menu.py
@@ -14,18 +14,17 @@ furry_menu = on_command("菜单02", aliases={"Furry菜单", "furry菜单"}, prio
 marry_menu = on_command("菜单03", aliases={"结婚菜单"}, priority=99, block=True)
 service_menu = on_command("服务条款", aliases={"用户协议"}, block=True)
 
-WORK_DATA = Path()
-ALL_MENU_MD = WORK_DATA / 'Markdown' / 'All_Menu.md'
-FURRY_MENU_MD = WORK_DATA / 'Markdown' / 'Furry_System.md'
-MAIN_MENU_MD = WORK_DATA / 'Markdown' / 'Main_System.md'
-SERVICE_MENU_MD = WORK_DATA / 'Markdown' / 'User_Agreement.md'
-MARRY_MENU_MD = WORK_DATA / 'Markdown' / 'Marry_Menu.md'
+ALL_MENU_MD = Path() / 'Markdown' / 'All_Menu.md'
+FURRY_MENU_MD = Path() / 'Markdown' / 'Furry_System.md'
+MAIN_MENU_MD = Path() / 'Markdown' / 'Main_System.md'
+SERVICE_MENU_MD = Path() / 'Markdown' / 'User_Agreement.md'
+MARRY_MENU_MD = Path() / 'Markdown' / 'Marry_Menu.md'
 
-ALL_MENU_PIC_DATA = WORK_DATA / 'data' / 'Menu' / 'All_Menu.png'
-FURRY_MENU_PIC_DATA = WORK_DATA / 'data' / 'Menu' / 'Furry_Menu.png'
-MAIN_MENU_PIC_DATA = WORK_DATA / 'data' / 'Menu' / 'Main_Menu.png'
-SERVICE_MENU_PIC_DATA = WORK_DATA / 'data' / 'Menu' / 'Service_Menu.png'
-MARRY_MENU_PIC_DATA = WORK_DATA / 'data' / 'Menu' / 'Marry_Menu.png'
+ALL_MENU_PIC_DATA = Path() / 'data' / 'Menu' / 'All_Menu.png'
+FURRY_MENU_PIC_DATA = Path() / 'data' / 'Menu' / 'Furry_Menu.png'
+MAIN_MENU_PIC_DATA = Path() / 'data' / 'Menu' / 'Main_Menu.png'
+SERVICE_MENU_PIC_DATA = Path() / 'data' / 'Menu' / 'Service_Menu.png'
+MARRY_MENU_PIC_DATA = Path() / 'data' / 'Menu' / 'Marry_Menu.png'
 
 
 @menu.handle()
@@ -53,6 +52,18 @@ async def marry_menu_func(matcher: Matcher, event: GroupMessageEvent, args: Mess
     await handle_menu_command(matcher, event, MARRY_MENU_MD, MARRY_MENU_PIC_DATA, args)
 
 
+async def handle_menu_command(matcher: Matcher, event: MessageEvent, md_path: Path, pic_path: Path, args: Message):
+    """通用的菜单命令处理函数"""
+    if args.to_rich_text():
+        await matcher.finish()
+
+    pic = await get_menu_pic(md_path=md_path, pic_path=pic_path)
+    await matcher.finish(
+        MessageSegment.reply(event.message_id) +
+        MessageSegment.image(pic)
+    )
+
+
 async def get_menu_pic(md_path: Path, pic_path: Path = None, width: int = 900) -> bytes:
     # 判断图片是否存在且比md文件新，否则重新生成
     if pic_path and pic_path.exists() and md_path.exists():
@@ -66,15 +77,3 @@ async def get_menu_pic(md_path: Path, pic_path: Path = None, width: int = 900) -
     with Image.open(io.BytesIO(image_bytes)) as img:
         img.save(pic_path)
     return image_bytes
-
-
-async def handle_menu_command(matcher: Matcher, event: MessageEvent, md_path: Path, pic_path: Path, args: Message):
-    """通用的菜单命令处理函数"""
-    if args.to_rich_text():
-        await matcher.finish()
-
-    pic = await get_menu_pic(md_path=md_path, pic_path=pic_path)
-    await matcher.finish(
-        MessageSegment.reply(event.message_id) +
-        MessageSegment.image(pic)
-    )
