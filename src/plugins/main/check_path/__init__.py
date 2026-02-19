c1827290f618acebae1a5b6c83200cf5470f8b1b
Harmless-Turtle <a1097740481@163.com>
2026-02-19 22:37:09 +0800

    重构main模块

---
+++ src/plugins/main/check_path/__init__.py
@@ -0,0 +1,3 @@
+from .check_path import _check_path
+__all__ = [_check_path]
+
---
+++ src/plugins/main/commands/__init__.py
@@ -0,0 +1,41 @@
+from . import commands
+from .commands import add_friend
+
+sign_in = commands.sign_in
+poke_check = commands.poke_check
+tarot = commands.tarot
+a_word = commands.a_word
+btfrk = commands.btfrk
+like = commands.like
+eat_what = commands.eat_what
+add_group = commands.add_group
+switch_add_group = commands.switch_add_group
+change_welcome = commands.change_welcome
+change_welcome_text = commands.change_welcome_text
+exit_change = commands.exit_change
+GroupExitMember = commands.GroupExitMember
+like_friend = commands.like_friend
+add_friend = commands.add_friend
+choice_friend = commands.choice_friend
+handle_group = commands.handle_group
+add_friend = commands.add_friend
+
+__all__ = [
+"sign_in",
+"poke_check",
+"tarot",
+"a_word",
+"btfrk",
+"like",
+"eat_what",
+"add_group",
+"switch_add_group",
+"change_welcome",
+"change_welcome_text",
+"exit_change",
+"GroupExitMember",
+"like_friend",
+"add_friend",
+"choice_friend",
+"handle_group"
+]
