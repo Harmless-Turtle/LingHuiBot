b0a954a9272234039a0ab9573cc79d27bc8134c9
Harmless-Turtle <a1097740481@163.com>
2026-02-24 14:54:35 +0800

    更新uv lock文件

--- src/plugins/marry_system/check_files.py
+++ src/plugins/marry_system/check_files.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from ..utils import ensure_files_exist
 
 # 定义Data存放路径并作为全局变量使用
-path_header = Path.cwd() / "data" / "Marry_System"
+path_header = Path.cwd() / "data" / "marry_system"
 # 定义全局变量方便处理
-marry_json_path = path_header / 'Marry.json'
-marry_count_path = path_header / 'Marry_Count.json'
+marry_json_path = path_header / 'marry_data.json'
+marry_count_path = path_header / 'marry_count.json'
 
 ensure_files_exist(
     [
