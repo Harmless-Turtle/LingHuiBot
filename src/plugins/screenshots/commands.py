1c5fd13ea397e30075c4eb48feaa44943a6b72d7
Harmless-Turtle <a1097740481@163.com>
2026-03-02 22:24:14 +0800

    Update:更新日志截屏功能

--- src/plugins/screenshots/commands.py
+++ src/plugins/screenshots/commands.py
@@ -1,5 +1,5 @@
 from nonebot import on_command
-from nonebot.rule import to_me
+from nonebot.permission import SUPERUSER
 
 # 定义命令
-screenshot_cmd = on_command("截屏", aliases={"截图"}, priority=10)
+screenshot_cmd = on_command("截屏", aliases={"截图"}, priority=10,permission=SUPERUSER)
