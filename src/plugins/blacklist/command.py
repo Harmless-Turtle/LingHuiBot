d1f311c083af878940612459d060862bba04f4e1
Harmless-Turtle <a1097740481@163.com>
2026-03-11 21:17:13 +0800

    Fix:修复凌辉su命令不可用的bug

--- src/plugins/blacklist/command.py
+++ src/plugins/blacklist/command.py
@@ -7,4 +7,4 @@ add_user_blacklist = on_command("添加用户黑名单", aliases={"加用户黑"
 del_user_blacklist = on_command("删除用户黑名单", aliases={"删用户黑", "移出用户黑名单"}, priority=10, permission=SUPERUSER)
 check_user_blacklist = on_command("查看用户黑名单",aliases={"查黑用户"},priority=10, permission=SUPERUSER)
 check_group_blacklist = on_command("查看群聊黑名单",aliases={"查群黑"},priority=10, permission=SUPERUSER)
-check_su = on_command("凌辉su",priority=10)
+check_su = on_command("su",priority=10)
