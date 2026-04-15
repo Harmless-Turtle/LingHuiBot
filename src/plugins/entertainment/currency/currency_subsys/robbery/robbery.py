c6485ba9b2375dd739d8e44209a6377390dbff27
Harmless-Turtle <a1097740481@163.com>
2026-04-15 20:53:07 +0800

    修复打劫命令不生效的问题。 更新菜单。 新建今日老婆的预留文件。 **停止维护：由于开发者需要忙学业，所以暂时停止更新与维护的相关日程。对此造成的不便，敬请谅解** **只有重大事故发生时，开发者才会重新修复bug等操作，但是修完之后也会立刻继续学业，十分抱歉。**

--- src/plugins/entertainment/currency/currency_subsys/robbery/robbery.py
+++ src/plugins/entertainment/currency/currency_subsys/robbery/robbery.py
@@ -24,7 +24,12 @@ async def _robbery(
         event: GroupMessageEvent,
         args:Message = CommandArg()
 ):
-    if not args.extract_plain_text() or not "@" in str(args): await matcher.finish()
+    plain_text = args.extract_plain_text().strip()
+    # 2. 检查消息段中是否包含真实的 AT
+    has_real_at = any(seg.type == "at" for seg in args)
+    # 3. 拦截逻辑：既没有真实 AT，也没有包含 "@" 符号的文本
+    if not (has_real_at or "@" in str(plain_text)):
+        await matcher.finish()
     target_id = None
     # 获取at的用户
     for msg_seg in event.original_message:
