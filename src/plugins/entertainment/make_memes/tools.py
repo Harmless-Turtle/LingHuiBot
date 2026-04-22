2caa31bccd4d55806c8f0ef89d73a8a5d866a2fc
Harmless-Turtle <a1097740481@163.com>
2026-04-22 21:15:00 +0800

    经过验证，表情制作已经可用。

--- src/plugins/entertainment/make_memes/tools.py
+++ src/plugins/entertainment/make_memes/tools.py
@@ -10,11 +10,10 @@ from meme_generator import (
 )
 from PIL import Image
 
-
 def _generate_meme_sync(meme_key: str, image_bytes: bytes):
     meme = get_meme(meme_key)
     if meme is None:
-        raise RuntimeError("meme not found: petpet")
+        return RuntimeError("meme not found: petpet")
 
     # 将传入的 bytes 数据转换为 meme-generator 可用的 Image 对象
     img = Image.open(io.BytesIO(image_bytes))
@@ -25,7 +24,7 @@ def _generate_meme_sync(meme_key: str, image_bytes: bytes):
     if isinstance(result, bytes):
         return result
     else:
-        raise RuntimeError(str(result))
+        return RuntimeError(str(result))
 
 async def generate_meme(meme_key: str, image_bytes: bytes):
     return await run_sync(_generate_meme_sync)(meme_key, image_bytes)
@@ -41,15 +40,17 @@ async def check_memes_func(meme_key: str):
 
     """
     meme = search_memes(meme_key, True)
-    if meme is None:
-        raise RuntimeError(f"meme not found: {meme}")
+    if not meme:
+        return RuntimeError(f"未从库中检索到表情：{meme}")
     if len(meme) >= 100:
-        raise RuntimeError(f"No args")
+        return RuntimeError(f"匹配结果过多，请输入更精确的关键词")
     return get_meme(f"{meme[0]}")
 
 async def download_avatar(user_id: str,target_dir: Path):
     async with httpx.AsyncClient() as client:
         resp = await client.get(f"http://q.qlogo.cn/headimg_dl?dst_uin={user_id}&spec=640&img_type=jpg")
+        print(f"status: {resp.status_code}, size: {len(resp.content)}, header: {resp.headers.get('content-type')}")
     with open(target_dir, 'wb') as f:
         f.write(resp.content)
         return
+
